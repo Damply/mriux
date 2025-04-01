de830fdab719911320007f87cd01a5182703c32b
Damply <31376489+Damply@users.noreply.github.com>
2025-03-31 19:00:16 -0600

    Update MiruExtension.js

--- MiruExtension.js
+++ MiruExtension.js
@@ -7,21 +7,27 @@ export default class MiruExtension extends AbstractSource {
   config = {}
 
   async single(options) {
+    console.log('MiruExtension: single() called', options);
     return await this.search1337x(options)
   }
 
   async batch(options) {
+    console.log('MiruExtension: batch() called', options);
     return await this.search1337x(options)
   }
 
   async movie(options) {
+    console.log('MiruExtension: movie() called', options);
     return await this.search1337x(options)
   }
 
   async search1337x(options) {
+    console.log('MiruExtension: search1337x() called with options:', options);
     const { titles } = options
     const query = titles[0]
     const url = `https://1337x.to/search/${encodeURIComponent(query)}/1/`
+
+    console.log('Fetching URL:', url);
 
     const response = await fetch(url)
     const html = await response.text()
@@ -29,6 +35,7 @@ export default class MiruExtension extends AbstractSource {
   }
 
   parse1337xResults(html) {
+    console.log('MiruExtension: parse1337xResults() called');
     const torrents = []
     const parser = new DOMParser()
     const doc = parser.parseFromString(html, 'text/html')
@@ -40,12 +47,13 @@ export default class MiruExtension extends AbstractSource {
       const seeders = parseInt(row.querySelector('td.coll-2').innerText)
       const leechers = parseInt(row.querySelector('td.coll-3').innerText)
       const size = row.querySelector('td.coll-4').innerText
-      const date = new Date()  // 1337x does not provide precise date info
+      const date = new Date()
       const hash = link.split('/').pop()
 
       torrents.push({ title, link, seeders, leechers, downloads: 0, hash, size, verified: true, date })
     })
 
+    console.log('Torrents found:', torrents);
     return torrents
   }
 }
